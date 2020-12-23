8233bb2547f5ff754e8c994985a8eeb7fd029364
Hao He <heh@pku.edu.cn>
2020-12-23 11:37:05 +0800

    Add utilities for further analysis use

--- util.py
+++ util.py
@@ -64,8 +64,23 @@ def get_issues_in_text(repo_name: str, text: str) -> List[Tuple[str, int]]:
 def select_projects_from_libraries_io() -> pd.DataFrame:
     if os.path.exists("cache/projects.csv"):
         return pd.read_csv("cache/projects.csv")
+    if not os.path.exists("cache"):
+        os.mkdir("cache")
+
     db = pymongo.MongoClient(MONGO_URL).migration_helper
-    projects = pd.DataFrame(db.lioProject.find())
+
+    projects = pd.DataFrame(list(db.lioRepository.find({
+        "hostType": "GitHub",
+        "fork": False,
+        "language": "Java",
+        "starsCount": {"$gte": 10},
+    })))
+    logging.debug(f"{len(projects)} non-fork GitHub Java projects with stars >= 10")
+
+    good_names = set(seq["repoName"].replace("_", "/") for seq in db.wocDepSeq3.find() if len(seq["seq"]) >= 1)
+    projects = projects[projects["nameWithOwner"].isin(good_names)]
+    logging.debug(f"{len(projects)} non-fork GitHub Java projects with stars >= 10 and one pom.xml file")
+
     projects.to_csv("cache/projects.csv", index=False)
     return projects
 
@@ -73,8 +88,15 @@ def select_projects_from_libraries_io() -> pd.DataFrame:
 def select_libraries_from_libraries_io() -> pd.DataFrame:
     if os.path.exists("cache/libraries.csv"):
         return pd.read_csv("cache/libraries.csv")
+    if not os.path.exists("cache"):
+        os.mkdir("cache")
+
     db = pymongo.MongoClient(MONGO_URL).migration_helper
-    libraries = pd.DataFrame(db.lioProject.find({"dependentRepositoriesCount": {"$ge": 10}}))
+    libraries = pd.DataFrame(list(db.lioProject.find({
+        "platform": "Maven",
+        "dependentRepositoriesCount": {"$gte": 10}
+    })))
+    logging.debug(f"{len(libraries)} libraries with dependent repository count >= 10")
     libraries.to_csv("cache/libraries.csv", index=False)
     return libraries
 
@@ -84,11 +106,46 @@ def select_rules(valid_libs: Set[str]) -> pd.DataFrame:
     return rules[rules["fromLib"].isin(valid_libs) & rules["toLib"].isin(valid_libs)]
 
 
+def select_dependency_changes(project_name: str, valid_libs: Set[str] = None) -> pd.DataFrame:
+    db = pymongo.MongoClient(MONGO_URL).migration_helper
+    results = []
+    for dep_seq in db.wocDepSeq3.find({"repoName": project_name.replace("/", "_")}):
+        for item in dep_seq["seq"]:
+            for change in item["versionChanges"]:
+                row = {
+                    "project": project_name,
+                    "commit": item["commit"],
+                    "file": dep_seq["fileName"],
+                    "lib1": "",
+                    "lib2": "",
+                    "ver1": "",
+                    "ver2": "",
+                }
+                if change.startswith("+"):
+                    row["lib2"], row["ver2"] = change[1:].split(" ")
+                elif change.startswith("-"):
+                    row["lib1"], row["ver1"] = change[1:].split(" ")
+                elif change.startswith(" "):
+                    row["lib1"] = row["lib2"] = change[1:].split(" ")[0]
+                    row["ver1"], row["ver2"] = change[1:].split(" ")[1].split("->")
+                results.append(row)
+    results = pd.DataFrame(results).fillna("")
+    if valid_libs is not None:
+        valid_libs.add("")
+        results = results[results["lib1"].isin(valid_libs) & results["lib2"].isin(valid_libs)]
+    return results
+
+
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
     res = get_issues_in_text("a/a", "cc#23 is abc GH-32 where abc/def#222 and a-c/D.f#333" +
                              " https://github.com/abc/def/issues/4444 sadwec " +
                              "https://github.com/abc./def-_/pull/5555")
     print(res)
+
     print(select_libraries_from_libraries_io())
     print(select_projects_from_libraries_io())
     print(select_rules(set(select_libraries_from_libraries_io()["name"])))
+    print(select_dependency_changes("square/okhttp"))
+    print(select_dependency_changes("square/okhttp", set(select_libraries_from_libraries_io()["name"])))
